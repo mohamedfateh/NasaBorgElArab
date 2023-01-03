87dd35e55de20d3258a3465bcdc53e3693a7ddaf
mohamed_alfateh <76884580+mohamedfateh@users.noreply.github.com>
2023-01-03 05:28:25 +0200

    Add files via upload

---
+++ nasa-home/rocket.js
@@ -0,0 +1,172 @@
+
+ function setNewElement0() {
+    document.getElementById("ichnasa0").src= "nasa-home/chnasa/11.png";
+
+ }
+function setImg0(){
+  document.getElementById("ichnasa0").src= "nasa-home/chnasa/chnasa0.png";
+
+}
+
+ function setNewElement1() {
+    document.getElementById("ichnasa1").src= "nasa-home/chnasa/22.png";
+
+ }
+function setImg1(){
+  document.getElementById("ichnasa1").src= "nasa-home/chnasa/chnasa1.png";
+
+}
+function setNewElement2() {
+   document.getElementById("ichnasa2").src= "nasa-home/chnasa/33.png";
+
+}
+function setImg2(){
+ document.getElementById("ichnasa2").src= "nasa-home/chnasa/chnasa2.png";
+
+}
+function setNewElement3() {
+   document.getElementById("ichnasa3").src= "nasa-home/chnasa/44.png";
+
+}
+function setImg3(){
+ document.getElementById("ichnasa3").src= "nasa-home/chnasa/chnasa3.png";
+
+} function setNewElement4() {
+    document.getElementById("ichnasa4").src= "nasa-home/chnasa/55.png";
+
+ }
+function setImg4(){
+  document.getElementById("ichnasa4").src= "nasa-home/chnasa/chnasa4.png";
+
+} function setNewElement5() {
+    document.getElementById("ichnasa5").src= "nasa-home/chnasa/66.png";
+
+ }
+function setImg5(){
+  document.getElementById("ichnasa5").src= "nasa-home/chnasa/chnasa5.png";
+
+} function setNewElement6() {
+    document.getElementById("ichnasa6").src= "nasa-home/chnasa/77.png";
+
+ }
+function setImg6(){
+  document.getElementById("ichnasa6").src= "nasa-home/chnasa/chnasa6.png";
+
+} function setNewElement7() {
+    document.getElementById("ichnasa7").src= "nasa-home/chnasa/88.png";
+
+ }
+function setImg7(){
+  document.getElementById("ichnasa7").src= "nasa-home/chnasa/chnasa7.png";
+
+} function setNewElement8() {
+    document.getElementById("ichnasa8").src= "nasa-home/chnasa/99.png";
+
+ }
+function setImg8(){
+  document.getElementById("ichnasa8").src= "nasa-home/chnasa/chnasa8.png";
+
+} function setNewElement9() {
+    document.getElementById("ichnasa9").src= "nasa-home/chnasa/1010.png";
+
+ }
+function setImg9(){
+  document.getElementById("ichnasa9").src= "nasa-home/chnasa/chnasa9.png";
+
+}
+function setNewElement10() {
+   document.getElementById("ichnasa10").src= "nasa-home/chnasa/1111.png";
+
+}
+function setImg10(){
+ document.getElementById("ichnasa10").src= "nasa-home/chnasa/newch1.png";
+
+}
+function setNewElement11() {
+   document.getElementById("ichnasa11").src= "nasa-home/chnasa/1212.png";
+
+}
+function setImg11(){
+ document.getElementById("ichnasa11").src= "nasa-home/chnasa/newch2.png";
+
+}
+function setNewElement12() {
+   document.getElementById("ichnasa12").src= "nasa-home/chnasa/1313.png";
+
+}
+function setImg12(){
+ document.getElementById("ichnasa12").src= "nasa-home/chnasa/newch3.png";
+
+}
+function setNewElement13() {
+   document.getElementById("ichnasa13").src= "nasa-home/chnasa/1414.png";
+
+}
+function setImg13(){
+ document.getElementById("ichnasa13").src= "nasa-home/chnasa/newch4.png";
+
+}
+function setNewElement14() {
+   document.getElementById("ichnasa14").src= "nasa-home/chnasa/1515.png";
+
+}
+function setImg14(){
+ document.getElementById("ichnasa14").src= "nasa-home/chnasa/newch5.png";
+
+}
+function setNewElement15() {
+   document.getElementById("ichnasa15").src= "nasa-home/chnasa/1616.png";
+
+}
+function setImg15(){
+ document.getElementById("ichnasa15").src= "nasa-home/chnasa/newch6.png";
+
+}
+function setNewElement16() {
+   document.getElementById("ichnasa16").src= "nasa-home/chnasa/1717.png";
+
+}
+function setImg16(){
+ document.getElementById("ichnasa16").src= "nasa-home/chnasa/newch7.png";
+
+}
+function setNewElement17() {
+   document.getElementById("ichnasa17").src= "nasa-home/chnasa/1818.png";
+
+}
+function setImg17(){
+ document.getElementById("ichnasa17").src= "nasa-home/chnasa/newch8.png";
+
+}
+function setNewElement18() {
+   document.getElementById("ichnasa18").src= "nasa-home/chnasa/1919.png";
+
+}
+function setImg18(){
+ document.getElementById("ichnasa18").src= "nasa-home/chnasa/newch9.png";
+
+}
+function setNewElement19() {
+   document.getElementById("ichnasa19").src= "nasa-home/chnasa/2020.png";
+
+}
+function setImg19(){
+ document.getElementById("ichnasa19").src= "nasa-home/chnasa/newch10.png";
+
+}
+function setNewElement20() {
+   document.getElementById("ichnasa20").src= "nasa-home/chnasa/2121.png";
+
+}
+function setImg20(){
+ document.getElementById("ichnasa20").src= "nasa-home/chnasa/newch11.png";
+
+}
+function setNewElement21() {
+   document.getElementById("ichnasa21").src= "nasa-home/chnasa/2222.png";
+
+}
+function setImg21(){
+ document.getElementById("ichnasa21").src= "nasa-home/chnasa/newch12.png";
+
+}
